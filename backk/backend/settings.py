19e4475123d74389b76e1a6c93aa0f9676cf2659
elowenfern <athik530@gmail.com>
2024-11-22 13:53:33 +0530

    static

--- backk/backend/settings.py
+++ backk/backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 
 
 
