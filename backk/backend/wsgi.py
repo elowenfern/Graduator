40a4cc70a47d9c93128997cb6337f7e57e57b4e4
elowenfern <athik530@gmail.com>
2024-11-26 14:40:26 +0530

    laast

--- backk/backend/wsgi.py
+++ backk/backend/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import logging
 from django.core.wsgi import get_wsgi_application
 
+
+logging.config.dictConfig(settings.LOGGING)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
