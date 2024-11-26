a175aa844914bb5aa9489137a4cca072660374df
elowenfern <athik530@gmail.com>
2024-11-26 13:14:33 +0530

    doot

--- backk/api/views.py
+++ backk/api/views.py
@@ -30,13 +30,6 @@ from django.conf import settings
 import logging
 import environ
 
-logger = logging.getLogger(__name__)
-logger.info(f"Twilio SID: {settings.TWILIO_ACCOUNT_SID}")
-logger.info(f"Twilio Token: {settings.TWILIO_AUTH_TOKEN}")
-logger.info(f"Twilio Phone Number: {settings.TWILIO_PHONE_NUMBER}")
-logger.info(f"Admin Phone: {settings.ADMIN_PHONE}")
-
-
 
 
 TWILIO_ACCOUNT_SID='ACed6bbcdbf60bff5ebbbfb8fd92ea1417e'
@@ -62,7 +55,7 @@ except Exception as e:
 
 env = environ.Env()
 environ.Env.read_env(env_file='.env') 
-client = Client(env('TWILIO_ACCOUNT_SID'), env('TWILIO_AUTH_TOKEN'))
+# client = Client(env('TWILIO_ACCOUNT_SID'), env('TWILIO_AUTH_TOKEN'))
 
 @csrf_exempt
 def send_whatsapp(request):
