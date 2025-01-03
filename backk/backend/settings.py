26c7fb959dbbe5174be2c7a659796d8cc271a5c7
elowenfern <athik530@gmail.com>
2025-01-03 11:18:36 +0530

    thirdjan

--- backk/backend/settings.py
+++ backk/backend/settings.py
@@ -123,14 +123,13 @@ CORS_ALLOW_HEADERS = [
 
 CORS_ALLOWED_ORIGINS = [
     'https://graduatorsacademy.com',
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
+    # "http://127.0.0.1:8000",
+    # "http://localhost:3000",
 ]
 
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost', 
+    # '127.0.0.1',
     'api.graduatorsacademy.com',
     ]
 
